749842b039a63ab575d42cfa4297e307800d207d
RadekKisiela <radek1997_97@o2.pl>
2023-12-12 07:18:00 +0100

    uptade

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './Navigation.module.css';
 
 export default function Navigation() {
+  const [isHomeActive, setIsHomeActive] = useState(false);
+  const [isMoviesActive, setIsMoviesActive] = useState(false);
+
+  const handleHomeClick = () => {
+    setIsHomeActive(true);
+    setIsMoviesActive(false);
+  };
+  const handleMoviesClick = () => {
+    setIsHomeActive(false);
+    setIsMoviesActive(true);
+  };
+
   return (
     <nav className={css.container}>
-      <NavLink to="/" className={css.navLink}>
+      <NavLink
+        to="/"
+        className={isHomeActive ? css.navLinkActive : css.navLink}
+        onClick={() => handleHomeClick()}
+      >
         Home
       </NavLink>
-      <NavLink to="/movies" className={css.navLink}>
+      <NavLink
+        to="/movies"
+        className={isMoviesActive ? css.navLinkActive : css.navLink}
+        onClick={() => handleMoviesClick()}
+      >
         Movies
       </NavLink>
     </nav>
